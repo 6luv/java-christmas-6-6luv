438c110c240d44877878947b4d59014e30f53cab
jmj005 <jmj00005@naver.com>
2023-11-09 22:14:46 +0900

    feat: 방문 날짜 문자 검증 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -7,12 +7,19 @@ public class InputView {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
         validateBlank(input);
+        validateNumeric(input);
         return Integer.parseInt(input);
     }
 
     private void validateBlank(String input) {
         if (input.trim().isBlank()) {
             throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
+        }
+    }
+
+    private void validateNumeric(String input) {
+        if (!input.matches("^[0-9]+$")) {
+            throw new IllegalArgumentException("[ERROR] 숫자만 입력할 수 있습니다.");
         }
     }
 }
