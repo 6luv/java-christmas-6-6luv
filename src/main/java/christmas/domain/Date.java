db00f4ab8edd0a97bf54d1556720aeb0be79028c
jmj005 <jmj00005@naver.com>
2023-11-09 22:41:19 +0900

    feat: 방문 날짜 범위 검증 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -1,9 +1,19 @@
 package christmas.domain;
 
 public class Date {
+    private static final int MIN_DATE = 1;
+    private static final int MAX_DATE = 31;
+
     private final int day;
 
     public Date(int day) {
+        validateRange(day);
         this.day = day;
+    }
+
+    private void validateRange(int day) {
+        if (MIN_DATE > day || day> MAX_DATE) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 }
