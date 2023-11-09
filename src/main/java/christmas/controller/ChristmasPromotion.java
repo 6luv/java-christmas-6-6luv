08e1bf4cee3b581bc514360324d5007e95b87dd9
jmj005 <jmj00005@naver.com>
2023-11-09 22:46:46 +0900

    feat: 방문 날짜 예외 발생 시 재입력 받기 구현

--- src/main/java/christmas/controller/ChristmasPromotion.java
+++ src/main/java/christmas/controller/ChristmasPromotion.java
@@ -3,6 +3,7 @@ package christmas.controller;
 import christmas.domain.Date;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+import java.util.Optional;
 
 public class ChristmasPromotion {
     private final InputView input;
@@ -15,6 +16,23 @@ public class ChristmasPromotion {
 
     public void run() {
         output.printStart();
-        Date date = new Date(input.readDate());
+        Date date = getDate();
+    }
+
+    private Date getDate() {
+        Optional<Date> date;
+        do {
+            date = checkDate();
+        } while(date.isEmpty());
+        return date.get();
+    }
+
+    private Optional<Date> checkDate() {
+        try {
+            return Optional.of(new Date(input.readDate()));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return Optional.empty();
+        }
     }
 }
