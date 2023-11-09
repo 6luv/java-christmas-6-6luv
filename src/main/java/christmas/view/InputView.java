84a5966f9cd7692bec722c5f7bf536b04cd5ddf7
jmj005 <jmj00005@naver.com>
2023-11-09 21:32:17 +0900

    feat: 방문 날짜 공백 검증 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -6,6 +6,13 @@ public class InputView {
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
+        validateBlank(input);
         return Integer.parseInt(input);
+    }
+
+    private void validateBlank(String input) {
+        if (input.trim().isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 공백은 입력할 수 없습니다.");
+        }
     }
 }
