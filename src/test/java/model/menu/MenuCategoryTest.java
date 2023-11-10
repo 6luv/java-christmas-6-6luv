960fcbccebc04f245658326ebbe11339074a5d19
jmj005 <jmj00005@naver.com>
2023-11-10 20:01:58 +0900

    refactor: 불필요한 import문 제거

--- src/test/java/model/menu/MenuCategoryTest.java
+++ src/test/java/model/menu/MenuCategoryTest.java
@@ -1,8 +1,6 @@
 package model.menu;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import christmas.domain.menu.MenuCategory;
