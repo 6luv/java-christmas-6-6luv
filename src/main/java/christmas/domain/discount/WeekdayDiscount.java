0d3a923868224f232607c9345b46eb87b5ed7d26
jmj005 <jmj00005@naver.com>
2023-11-11 02:26:38 +0900

    fix: 평일 할인 로직 수정

--- src/main/java/christmas/domain/discount/WeekdayDiscount.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscount.java
@@ -1,25 +1,32 @@
 package christmas.domain.discount;
 
 import christmas.domain.menu.Dessert;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Map;
 
 public class WeekdayDiscount {
     public static int calculateDiscount(int date, Map<String, String> order) {
-        if (isWeekly(date)) {
+        LocalDate localDate = LocalDate.of(2023, 12, date);
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        if (!isWeekday(dayOfWeek)) {
             return 0;
         }
         int dessertCount = order.entrySet()
                 .stream()
                 .filter(entry -> Arrays.stream(Dessert.values())
-                        .anyMatch(menu -> menu.hasMenu(entry.getKey())))
+                        .anyMatch(dessert -> dessert.hasMenu(entry.getKey())))
                 .mapToInt(entry -> Integer.parseInt(entry.getValue()))
-                .sum();
+                .findFirst()
+                .orElse(0);
+
         return dessertCount * 2023;
     }
 
-    private static boolean isWeekly(int date) {
-        return date >= Calendar.SUNDAY && date <= Calendar.THURSDAY;
+    private static boolean isWeekday(DayOfWeek dayOfWeek) {
+        return dayOfWeek.getValue() >= DayOfWeek.SUNDAY.getValue()
+                || dayOfWeek.getValue() <= DayOfWeek.THURSDAY.getValue();
     }
 }
---
+++ src/main/java/christmas/domain/discount/WeekendDiscount.java
@@ -0,0 +1,11 @@
+package christmas.domain.discount;
+
+import java.util.Map;
+
+public class WeekendDiscount {
+//    public static int calculateDiscount(int date, Map<String, String> order) {
+//        if (isWeekend()) {
+//
+//        }
+//    }
+}
