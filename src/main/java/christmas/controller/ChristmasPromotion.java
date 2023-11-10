4fb8e8c1e055f3632bfb9bc3739a4ac5445d4ce4
jmj005 <jmj00005@naver.com>
2023-11-11 02:32:04 +0900

    feat: 주말 할인 계산하기 구현

--- src/main/java/christmas/controller/ChristmasPromotion.java
+++ src/main/java/christmas/controller/ChristmasPromotion.java
@@ -3,6 +3,7 @@ package christmas.controller;
 import christmas.domain.Date;
 import christmas.domain.discount.DdayDiscount;
 import christmas.domain.discount.WeekdayDiscount;
+import christmas.domain.discount.WeekendDiscount;
 import christmas.domain.menu.MenuCategory;
 import christmas.view.InputView;
 import christmas.view.OutputView;
@@ -31,6 +32,7 @@ public class ChristmasPromotion {
 
         DdayDiscount.calculateDiscount(date.getDay());
         WeekdayDiscount.calculateDiscount(date.getDay(), order);
+        WeekendDiscount.calculateDiscount(date.getDay(), order);
     }
 
     private Date getDate() {
--- src/main/java/christmas/domain/discount/WeekendDiscount.java
+++ src/main/java/christmas/domain/discount/WeekendDiscount.java
@@ -1,11 +1,32 @@
 package christmas.domain.discount;
 
+import christmas.domain.menu.MainDish;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Map;
 
 public class WeekendDiscount {
-//    public static int calculateDiscount(int date, Map<String, String> order) {
-//        if (isWeekend()) {
-//
-//        }
-//    }
+    public static int calculateDiscount(int date, Map<String, String> order) {
+        LocalDate localDate = LocalDate.of(2023, 12, date);
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        if (!isWeekend(dayOfWeek)) {
+            return 0;
+        }
+        int mainDishCount = order.entrySet()
+                .stream()
+                .filter(entry -> Arrays.stream(MainDish.values())
+                        .anyMatch(mainDish -> mainDish.hasMenu(entry.getKey())))
+                .mapToInt(entry -> Integer.parseInt(entry.getValue()))
+                .findFirst()
+                .orElse(0);
+
+        return mainDishCount * 2023;
+    }
+
+    private static boolean isWeekend(DayOfWeek dayOfWeek) {
+        return dayOfWeek.getValue() == DayOfWeek.FRIDAY.getValue()
+                || dayOfWeek.getValue() == DayOfWeek.SATURDAY.getValue();
+    }
 }
