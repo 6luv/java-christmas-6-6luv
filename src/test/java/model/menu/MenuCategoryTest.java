d1526c56f142dc9d97bd9aee098ebb8199470a78
jmj005 <jmj00005@naver.com>
2023-11-12 02:23:33 +0900

    refactor: 메뉴 이름 유효성 테스트 리팩토링

--- src/test/java/model/menu/MenuCategoryTest.java
+++ src/test/java/model/menu/MenuCategoryTest.java
@@ -1,9 +1,9 @@
 package model.menu;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import christmas.domain.menu.MenuCategory;
+import christmas.domain.menu.Menu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,23 +11,24 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class MenuCategoryTest {
     @ParameterizedTest
     @CsvSource({
-            "타파스",
-            "해산물파스타",
-            "제로콜라"
+            "타파스, 1",
+            "해산물파스타, 1",
+            "제로콜라, 1"
     })
     @DisplayName("메뉴 이름 유효성 성공 테스트")
-    void isMenuValidTest(String menuName) {
-        assertTrue(() -> MenuCategory.isMenuValid(menuName));
+    void isMenuValidTest(String menuName, int menuCount) {
+        assertDoesNotThrow(() -> new Menu(menuName, menuCount));
     }
 
     @ParameterizedTest
     @CsvSource({
-            "치킨",
-            "피자",
-            "사이다"
+            "치킨, 1",
+            "피자, 1",
+            "사이다, 1"
     })
     @DisplayName("메뉴 이름 유효성 실패 테스트")
-    void isMenuInValidTest(String menuName) {
-        assertFalse(MenuCategory.isMenuValid(menuName));
+    void isMenuInValidTest(String menuName, int menuCount) {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Menu(menuName, menuCount));
     }
 }
