eb23dc03c002b86aedc702abc78b70661692d491
jmj005 <jmj00005@naver.com>
2023-11-12 02:09:21 +0900

    refactor: MenuCategory에서 모든 메뉴 클래스를 가져오는 메서드 구현

--- src/main/java/christmas/domain/menu/MenuCategory.java
+++ src/main/java/christmas/domain/menu/MenuCategory.java
@@ -1,8 +1,5 @@
 package christmas.domain.menu;
 
-import java.util.Arrays;
-import java.util.Map;
-
 public enum MenuCategory {
     APPETIZER(Appetizer.class),
     MAIN_DISH(MainDish.class),
@@ -15,28 +12,7 @@ public enum MenuCategory {
         this.menuClass = menuClass;
     }
 
-    public static boolean isMenuValid(String menuName) {
-        return Arrays.stream(MenuCategory.values())
-                .anyMatch(category -> category.isMenuInCategory(menuName));
-    }
-
-    private boolean isMenuInCategory(String menuName) {
-        return Arrays.stream(menuClass.getEnumConstants())
-                .anyMatch(menuItem -> menuItem.hasMenu(menuName));
-    }
-
-    public static int calculatePrice(Map<String, String> order) {
-        return order.entrySet()
-                .stream()
-                .mapToInt(entry -> calculateSingleMenuPrice(entry.getKey(), entry.getValue()))
-                .sum();
-    }
-
-    private static int calculateSingleMenuPrice(String menu, String quantity) {
-        return Arrays.stream(MenuCategory.values())
-                .flatMap(category -> Arrays.stream(category.menuClass.getEnumConstants()))
-                .filter(menuItem -> menuItem.hasMenu(menu))
-                .mapToInt(menuItem -> menuItem.calculatePrice(quantity))
-                .sum();
+    public Class<? extends MenuItem> getMenuClass() {
+        return menuClass;
     }
 }
