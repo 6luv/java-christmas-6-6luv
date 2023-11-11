be05d619a27bde3500738eb3dda35882910d7381
jmj005 <jmj00005@naver.com>
2023-11-12 02:37:44 +0900

    test: 메뉴 개수 검증 실패 테스트 추가

--- src/test/java/model/menu/MenuTest.java
+++ src/test/java/model/menu/MenuTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class MenuCategoryTest {
+public class MenuTest {
     @ParameterizedTest
     @CsvSource({
             "타파스, 1",
@@ -16,7 +16,7 @@ public class MenuCategoryTest {
             "제로콜라, 1"
     })
     @DisplayName("메뉴 이름 유효성 성공 테스트")
-    void isMenuValidTest(String menuName, int menuCount) {
+    void validMenuNameTest(String menuName, int menuCount) {
         assertDoesNotThrow(() -> new Menu(menuName, menuCount));
     }
 
@@ -27,7 +27,19 @@ public class MenuCategoryTest {
             "사이다, 1"
     })
     @DisplayName("메뉴 이름 유효성 실패 테스트")
-    void isMenuInValidTest(String menuName, int menuCount) {
+    void invalidMenuNameTest(String menuName, int menuCount) {
+        assertThrows(IllegalArgumentException.class,
+                () -> new Menu(menuName, menuCount));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "제로콜라, 0",
+            "타파스, 21",
+            "해산물파스타, 30"
+    })
+    @DisplayName("메뉴 개수 검증 실패 테스트")
+    void invalidMenuCountTest(String menuName, int menuCount) {
         assertThrows(IllegalArgumentException.class,
                 () -> new Menu(menuName, menuCount));
     }
