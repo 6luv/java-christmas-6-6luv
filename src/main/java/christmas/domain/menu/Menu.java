b9e46f6443419a322c7a6f93a84013584d31975e
jmj005 <jmj00005@naver.com>
2023-11-12 02:03:23 +0900

    refactor: 단일 메뉴 관리를 위한 Menu 클래스 생성

--- src/main/java/christmas/domain/menu/Menu.java
+++ src/main/java/christmas/domain/menu/Menu.java
@@ -1,2 +1,38 @@
-package christmas.domain.menu;public class Menu {
+package christmas.domain.menu;
+
+import java.util.Arrays;
+
+public class Menu {
+    private final String menuName;
+    private final int menuCount;
+
+    public Menu(String menuName, int menuCount) {
+        validateMenuName(menuName);
+        validateMenuCount(menuCount);
+        this.menuName = menuName;
+        this.menuCount = menuCount;
+    }
+
+    private void validateMenuName(String menuName) {
+        if (!hasMenuName(menuName)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean hasMenuName(String menuName) {
+        return Arrays.stream(MenuCategory.values())
+                .flatMap(menuCategory -> Arrays.stream(menuCategory.getMenuClass().getEnumConstants())
+                        .map(MenuItem.class::cast))
+                .anyMatch(menu -> menu.hasMenu(menuName));
+    }
+
+    private void validateMenuCount(int menuCount) {
+        if (!isMenuCountValidRange(menuCount)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean isMenuCountValidRange(int menuCount) {
+        return 1 <= menuCount && menuCount <= 31;
+    }
 }
