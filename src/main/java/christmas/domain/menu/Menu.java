c6535313f72b616165cfa5b0be32b881914a27d2
jmj005 <jmj00005@naver.com>
2023-11-14 00:27:40 +0900

    refactor: 메뉴 개수 검증하기 예외 메시지 수정

--- src/main/java/christmas/domain/menu/Menu.java
+++ src/main/java/christmas/domain/menu/Menu.java
@@ -26,7 +26,7 @@ public class Menu {
 
     private void validateMenuCount(int menuCount) {
         if (!isMenuCountValidRange(menuCount)) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
         }
     }
 
