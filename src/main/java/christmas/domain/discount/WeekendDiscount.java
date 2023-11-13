2dda1fbfbfe5a23d74c843011a0209ccf3b579d6
jmj005 <jmj00005@naver.com>
2023-11-13 17:34:51 +0900

    refactor: 주말 할인을 위한 클래스 생성

--- src/main/java/christmas/domain/discount/WeekendDiscount.java
+++ src/main/java/christmas/domain/discount/WeekendDiscount.java
@@ -1,2 +1,24 @@
-package christmas.domain.discount;public class WeekendDiscount {
+package christmas.domain.discount;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class WeekendDiscount implements DiscountCalculator {
+    @Override
+    public DiscountInfo calculateDiscount(DiscountContext context) {
+        LocalDate localDate = LocalDate.of(2023, 12, context.getDate());
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+
+        int discount = 0;
+
+        if (isWeekend(dayOfWeek)) {
+            discount = context.getMainDishCount() * 2023;
+        }
+        return new DiscountInfo("주말 할인", discount);
+    }
+
+    private static boolean isWeekend(DayOfWeek dayOfWeek) {
+        return dayOfWeek.getValue() == DayOfWeek.FRIDAY.getValue()
+                || dayOfWeek.getValue() == DayOfWeek.SATURDAY.getValue();
+    }
 }
