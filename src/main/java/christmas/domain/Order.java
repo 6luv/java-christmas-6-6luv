6584fa9ae222861576d1a31958b27f8255da6394
jmj005 <jmj00005@naver.com>
2023-11-14 00:30:15 +0900

    feat: 총 주문 개수 검증하기 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -7,10 +7,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Order {
+
     private final List<Menu> order;
 
     public Order(List<Menu> order) {
         validateUnique(order);
+        validateOrderCount(order);
         validateAllOrdersAreBeverage(order);
         this.order = order;
     }
@@ -25,6 +27,16 @@ public class Order {
     private void validateAllOrdersAreBeverage(List<Menu> order) {
         if (Beverage.isEveryMenuBeverage(order)) {
             throw new IllegalArgumentException("[ERROR] 음료만 주문 시, 주문할 수 없습니다.");
+        }
+    }
+
+    private void validateOrderCount(List<Menu> order) {
+        int menuCount = order.stream()
+                .mapToInt(Menu::getMenuCount)
+                .sum();
+
+        if (menuCount > 20) {
+            throw new IllegalArgumentException("[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.");
         }
     }
 
