413da47576f8ee8635021e4aebdb475c971eba47
jmj005 <jmj00005@naver.com>
2023-11-14 15:31:30 +0900

    feat: 크리스마스 프로모션 흐름 로직 작성

--- src/main/java/christmas/controller/ChristmasPromotion.java
+++ src/main/java/christmas/controller/ChristmasPromotion.java
@@ -2,7 +2,13 @@ package christmas.controller;
 
 import christmas.domain.Date;
 import christmas.domain.Order;
+import christmas.domain.benefit.BenefitContext;
+import christmas.domain.benefit.Benefits;
+import christmas.domain.benefit.giveaway.Champagne;
+import christmas.domain.menu.Dessert;
+import christmas.domain.menu.MainDish;
 import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuCategory;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.Arrays;
@@ -19,9 +25,22 @@ public class ChristmasPromotion {
 
     public void run() {
         output.printStart();
+        Date date = processDate();
+        Order order = processOrder();
+
+        int totalPrice = order.getTotalPrice();
+        output.printTotalPrice(totalPrice);
+        output.printGiveaway(Champagne.getGiveaway(totalPrice));
+
+        BenefitContext context = generateContext(order, date.getDay(), totalPrice);
+        int benefitsAmount = processBenefit(context);
+        int giveawayAmount = processGiveaway(context);
+    }
+
+    private Date processDate() {
         Date date = getDate();
-        Order order = getOrder();
         output.printDate(date);
+        return date;
     }
 
     private Date getDate() {
@@ -32,6 +51,12 @@ public class ChristmasPromotion {
                 System.out.println(e.getMessage());
             }
         }
+    }
+
+    private Order processOrder() {
+        Order order = getOrder();
+        output.printOrder(order.getOrder());
+        return order;
     }
 
     private Order getOrder() {
@@ -48,7 +73,27 @@ public class ChristmasPromotion {
     private List<Menu> parseOrder(String order) {
         return Arrays.stream(order.split(","))
                 .map(menuItem -> menuItem.split("-"))
-                .map(menu -> new Menu(menu[0], Integer.parseInt(menu[1])))
+                .map(menu -> new Menu(menu[0], Integer.parseInt(menu[1]), MenuCategory.determineMenuCategory(menu[0])))
                 .toList();
+    }
+
+    private BenefitContext generateContext(Order order, int date, int totalPrice) {
+        int dessertCount = Dessert.calculateDessertCount(order);
+        int mainDishCount = MainDish.calculateMainDishCount(order);
+        return new BenefitContext(date, dessertCount, mainDishCount, totalPrice);
+    }
+
+    private int processBenefit(BenefitContext context) {
+        Benefits benefits = Benefits.calculateBenefits(context);
+        output.printBenefits(benefits.getBenefits());
+
+        int benefitsAmount = Benefits.calculateBenefitAmount(benefits);
+        output.printBenefitAmount(benefitsAmount);
+        return benefitsAmount;
+    }
+
+    private int processGiveaway(BenefitContext context) {
+        Champagne champagne = new Champagne();
+        return champagne.calculateBenefit(context).getAmount();
     }
 }
