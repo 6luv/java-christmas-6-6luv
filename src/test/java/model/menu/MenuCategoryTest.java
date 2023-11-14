236ffac525acdaa49c7aa9c3940452e664124052
jmj005 <jmj00005@naver.com>
2023-11-14 19:00:50 +0900

    test: 메뉴 카테고리 반환 테스트 추가

---
+++ src/test/java/model/menu/MenuCategoryTest.java
@@ -0,0 +1,44 @@
+package model.menu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import christmas.domain.menu.MenuCategory;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class MenuCategoryTest {
+    @ParameterizedTest
+    @MethodSource("validMenuNamesAndCategories")
+    @DisplayName("메뉴 카테고리 반환 성공 테스트")
+    void determineCategoryTest(String menuName, MenuCategory expectedCategory) {
+        MenuCategory actualCategory = MenuCategory.determineMenuCategory(menuName);
+        assertEquals(expectedCategory, actualCategory);
+    }
+
+    private static Stream<Arguments> validMenuNamesAndCategories() {
+        return Stream.of(
+                Arguments.of("타파스", MenuCategory.APPETIZER),
+                Arguments.of("티본스테이크", MenuCategory.MAIN_DISH),
+                Arguments.of("아이스크림", MenuCategory.DESSERT),
+                Arguments.of("레드와인", MenuCategory.BEVERAGE)
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "치킨",
+            "피자",
+            "사이다"
+    })
+    @DisplayName("메뉴 카테고리 반환 실패 테스트")
+    void determineCategoryTest(String menuName) {
+        assertThatThrownBy(() -> MenuCategory.determineMenuCategory(menuName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 메뉴입니다. 다시 입력해 주세요.");
+    }
+}
