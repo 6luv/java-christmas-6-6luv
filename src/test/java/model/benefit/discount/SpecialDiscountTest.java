a3cb3b0ea6a0909985453bce4c0d34b38ef08bba
jmj005 <jmj00005@naver.com>
2023-11-14 19:56:03 +0900

    test: 특별 할인 테스트 추가

---
+++ src/test/java/model/benefit/discount/SpecialDiscountTest.java
@@ -0,0 +1,30 @@
+package model.benefit.discount;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import christmas.domain.benefit.BenefitContext;
+import christmas.domain.benefit.BenefitInfo;
+import christmas.domain.benefit.discount.SpecialDiscount;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SpecialDiscountTest {
+    @ParameterizedTest
+    @CsvSource({
+            "1, 0",
+            "3, 1000",
+            "10, 1000",
+            "24, 1000",
+            "25, 1000",
+            "26, 0"
+    })
+    @DisplayName("특별 할인 테스트")
+    void calculateBenefit(int date, int expectedDiscount) {
+        BenefitContext context = new BenefitContext(date, 0, 0, 0);
+        SpecialDiscount specialDiscount = new SpecialDiscount();
+
+        BenefitInfo info = specialDiscount.calculateBenefit(context);
+        assertEquals(expectedDiscount, info.getAmount());
+    }
+}
