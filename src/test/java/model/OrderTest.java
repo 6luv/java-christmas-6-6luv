9ad8a80174114e3c72df29cd3fabfe33ee1f2069
jmj005 <jmj00005@naver.com>
2023-11-14 21:33:04 +0900

    refactor: 메뉴 중복 테스트 리팩토링

--- src/test/java/model/OrderTest.java
+++ src/test/java/model/OrderTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import christmas.domain.Order;
 import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuCategory;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -12,21 +13,21 @@ import org.junit.jupiter.api.Test;
 
 public class OrderTest {
     @Test
-    @DisplayName("메뉴 중복 실패 테스트")
+    @DisplayName("메뉴가 중복된 경우 테스트")
     void invalidOrderUniqueTest() {
         List<Menu> order = new ArrayList<>();
-        order.add(new Menu("제로콜라", 1));
-        order.add(new Menu("제로콜라", 3));
+        order.add(new Menu("제로콜라", 1, MenuCategory.BEVERAGE));
+        order.add(new Menu("제로콜라", 3, MenuCategory.BEVERAGE));
         assertThrows(IllegalArgumentException.class,
                 () -> new Order(order));
     }
 
     @Test
-    @DisplayName("메뉴 중복 성공 테스트")
+    @DisplayName("메뉴가 중복되지 않은 테스트")
     void validOrderUniqueTest() {
         List<Menu> order = new ArrayList<>();
-        order.add(new Menu("제로콜라", 1));
-        order.add(new Menu("해산물파스타", 2));
+        order.add(new Menu("제로콜라", 1, MenuCategory.BEVERAGE));
+        order.add(new Menu("해산물파스타", 2, MenuCategory.MAIN_DISH));
         assertDoesNotThrow(() -> new Order(order));
     }
 }
