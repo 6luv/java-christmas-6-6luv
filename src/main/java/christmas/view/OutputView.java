8900863028becd5590841b932992b5080fbc3e49
jmj005 <jmj00005@naver.com>
2023-11-15 17:49:38 +0900

    refactor: 불필요한 메서드 제거

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -85,8 +85,4 @@ public class OutputView {
         System.out.printf(LINE_SEPARATOR + "<%d월 이벤트 배지>" + LINE_SEPARATOR, EVENT_MONTH);
         System.out.println(eventBadge);
     }
-
-    public void printErrorMessage(String message) {
-        System.out.println(message);
-    }
 }
